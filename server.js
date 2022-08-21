4c7b90ced97826ad54a0b2969bf4987a8ff5b9c6
Gourab Goswami <gourab.goswami@ongraphmail.com>
2022-08-21 13:52:55 +0530

    update server.js v2

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/front-end'));
 
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
+    });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
