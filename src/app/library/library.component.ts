a5b249b7ca6457de113a7ba38d9add95ad240017
Gourab Goswami <gourab.goswami@ongraphmail.com>
2022-08-26 22:28:08 +0530

    Minor change

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -4,49 +4,38 @@ import { BookService } from '../services/book.service';
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.css']
+  styleUrls: ['./library.component.css'],
 })
 export class LibraryComponent implements OnInit {
-  errorMsg!:String;
-  isList : Boolean = false;
+  errorMsg!: String;
+  isList: Boolean = false;
   isError = false;
-  books: any =[];
-  constructor(public bookService:BookService) { }
+  books: any = [];
+  max: Number = 5;
+  isReadonly: Boolean = true;
+  starWidth: number = 0;
+favourites: any =[];
+  constructor(public bookService: BookService) {}
 
   ngOnInit(): void {
-    this.bookService.getLibraryBooks().pipe(
-      catchError((error) => {
-        this.errorMsg = 'Something went wrong! Could not fetch books!';
-        this.isError = true;
-        console.log(error.message);
-        return of(false);
-      })).subscribe((res) => {
-      if (res)
-        this.books = res;
-    });
+    this.bookService
+      .getLibraryBooks()
+      .pipe(
+        catchError((error) => {
+          this.errorMsg = 'Something went wrong! Could not fetch books!';
+          this.isError = true;
+          console.log(error.message);
+          return of(false);
+        })
+      )
+      .subscribe((res) => {
+        if (res) this.books = res;
+      });
   }
-
-  // Get the elements with class="column"
-  elements = document.getElementsByClassName('view-group') as HTMLCollectionOf<HTMLElement>
-
-  // List View
-  listView() {
-    for (let i = 0; i < this.elements.length; i++) {
-      this.elements[i].style.display = "grid";
-      this.elements[i].style.width = "300%";
-    }
-  }
-
-  // Grid View
-  gridView() {
-    for (let i = 0; i < this.elements.length; i++) {
-      this.elements[i].style.display = "flex";
-    }
-  }
-
-  myFunction() {
-    var element = document.getElementById("myDIV");
-    element?.classList.toggle("list");
+  
+  changeView() {
+    var element = document.getElementById('myDIV');
+    element?.classList.toggle('list');
     this.isList = !this.isList;
- }
+  }
 }
